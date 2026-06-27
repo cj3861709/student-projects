4482a4aa216cf91307aa03eeedd8db5e1a15d511
cj3861709 <2757422668@qq.com>
2026-06-27 20:11:57 +0800

    Add: digital-business-card/source/settings.gradle.kts

---
+++ digital-business-card/source/settings.gradle.kts
@@ -0,0 +1,27 @@
+// 项目设置文件，配置插件仓库、依赖仓库及模块引用
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "EmptyActivity"
+include(":app")
